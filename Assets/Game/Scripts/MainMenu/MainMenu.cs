b0c5082efb6e08b08f24496819f1f4834f02d562
MaksimBaturin <baturbaya@yandex.ru>
2025-05-19 23:25:45 +0300

    Улучшил кнопки, навигацию

--- Assets/Game/Scripts/MainMenu/MainMenu.cs
+++ Assets/Game/Scripts/MainMenu/MainMenu.cs
@@ -8,6 +8,15 @@ public class MainMenu : MonoBehaviour
     public void Start_OnCLick()
     {
         Destroy(gameObject);
+        //Тут надо вызывать окно с выбором кол-ва игроков ников и тд, потом формируем это
+        Player[] players =
+        {
+            new Player("Maxon", Color.red),
+            new Player("Asan", Color.blue),
+            new Player("Skiba", Color.green),
+        };
+        //и вызываем это
+        Bootstrap.Instance.GameInit(players);
     }
     public void Quit_OnClick()
     {
